a99dfc43ade3c49445ca12e37ece3baecb26a3cc
Krit Uthaisang <krit34830@gmail.com>
2021-11-25 13:07:37 +0700

    Update Database

--- database_info.sh
+++
@@ -1,1 +0,0 @@
-RES_GROUP=Boom ACCT_NAME=obd export ACCOUNT_URI=$(az cosmosdb show --resource-group $RES_GROUP --name $ACCT_NAME --query documentEndpoint --output tsv) export ACCOUNT_KEY=$(az cosmosdb list-keys --resource-group $RES_GROUP --name $ACCT_NAME --query primaryMasterKey --output tsv)
