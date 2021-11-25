1bee01dab6cb5768fc439dc70e006331e8a060b6
Krit Uthaisang <krit34830@gmail.com>
2021-11-26 02:33:12 +0700

    Database class updated

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', '[YOUR ENDPOINT]'),
-    'master_key': os.environ.get('ACCOUNT_KEY', '[YOUR KEY]'),
-    'database_id': os.environ.get('COSMOS_DATABASE', '[YOUR DATABASE]'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', '[YOUR CONTAINER]'),
+    'host': os.environ.get('ACCOUNT_HOST', 'https://obd.documents.azure.com:443/'),
+    'master_key': os.environ.get('ACCOUNT_KEY', 'whXTSvmqtQavVKAIOA3u98hekaI4tW77VjHXc16G9wFTIfYmYA8Osf59gL6aq748JmaWH0orZg5KdXc756c5Yg=='),
+    'database_id': os.environ.get('COSMOS_DATABASE', 'obd_database'),
+    'container_id': os.environ.get('COSMOS_CONTAINER', 'data'),
 }
--- database_management.py
+++ database_management.py
@@ -1,122 +1,80 @@
 import azure.cosmos.cosmos_client as cosmos_client
 import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
 import config
 
-# ----------------------------------------------------------------------------------------------------------
-# Prerequistes -
-#
-# 1. An Azure Cosmos account -
-#    https://docs.microsoft.com/azure/cosmos-db/create-sql-api-python#create-a-database-account
-#
-# 2. Microsoft Azure Cosmos PyPi package -
-#    https://pypi.python.org/pypi/azure-cosmos/
-# ----------------------------------------------------------------------------------------------------------
-# Sample - demonstrates the basic CRUD operations on a Database resource for Azure Cosmos
-#
-# 1. Query for Database (QueryDatabases)
-#
-# 2. Create Database (CreateDatabase)
-#
-# 3. Get a Database by its Id property (ReadDatabase)
-#
-# 4. List all Database resources on an account (ReadDatabases)
-#
-# 5. Delete a Database given its Id property (DeleteDatabase)
-# ----------------------------------------------------------------------------------------------------------
 
 HOST = config.settings['host']
 MASTER_KEY = config.settings['master_key']
 DATABASE_ID = config.settings['database_id']
+CONTAINER_ID = config.settings['container_id']
 
-def find_database(client, id):
-    print('1. Query for Database')
+class Cosmos_DB():
 
-    databases = list(client.query_databases({
-        "query": "SELECT * FROM r WHERE r.id=@id",
-        "parameters": [
-            { "name":"@id", "value": id }
-        ]
-    }))
+    def __init__(self,host,master_key,database_id,container_id):
 
-    if len(databases) > 0:
-        print('Database with id \'{0}\' was found'.format(id))
-    else:
-        print('No database with id \'{0}\' was found'. format(id))
+        #Create Client
+        self.client = cosmos_client.CosmosClient(host, {'masterKey': master_key} )
 
+        # setup database 
+        try:
+            self.database = self.client.create_database(database_id)
+        except exceptions.CosmosResourceExistsError:
+            self.database = self.client.get_database_client(database_id)
 
-def create_database(client, id):
-    print("\n2. Create Database")
-
-    try:
-        client.create_database(id=id)
-        print('Database with id \'{0}\' created'.format(id))
-
-    except exceptions.CosmosResourceExistsError:
-        print('A database with id \'{0}\' already exists'.format(id))
-
-
-def read_database(client, id):
-    print("\n3. Get a Database by id")
-
-    try:
-        database = client.get_database_client(id)
-        print('Database with id \'{0}\' was found, it\'s link is {1}'.format(id, database.database_link))
-
-    except exceptions.CosmosResourceNotFoundError:
-        print('A database with id \'{0}\' does not exist'.format(id))
+        # setup container 
+        try:
+            self.container = self.database.create_container(id=CONTAINER_ID, partition_key=PartitionKey(path='/id'))
+        except exceptions.CosmosResourceExistsError:
+            self.container = self.database.get_container_client(CONTAINER_ID)
+        except exceptions.CosmosHttpResponseError:
+            raise
+    
+    def read_item(self, doc_id):
+        print('\nReading Item by Id\n')
+        # Note that Reads require a partition key to be spcified.
+        response = self.container.read_item(item=doc_id, partition_key=doc_id)
+        print(response)
+       
 
 
-def list_databases(client):
-    print("\n4. List all Databases on an account")
+    def read_items(self):
+        print('\nReading all items in a container\n')
 
-    print('Databases:')
+        # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
+        #       Important to handle throttles whenever you are doing operations such as this that might
+        #       result in a 429 (throttled request)
+        item_list = list(self.container.read_all_items(max_item_count=10))
+        print('Found {0} items'.format(item_list.__len__()))
+        for doc in item_list:
+            print('Item Id: {0}'.format(doc.get('id')))
+    
+    def upsert_item(self,data):
+        print('Upserting Items')  
+        self.container.upsert_item(body=data)
+    
+    def delete_item(self, doc_id,sql):
+        #doc_id is the id of the data that you want to delete in the table, must be from the Partition Key table
+        for item in self.container.query_items(
+            query= sql,
+            enable_cross_partition_query=True):
+            self.container.delete_item(item, partition_key=doc_id)
 
-    databases = list(client.list_databases())
-
-    if not databases:
-        return
-
-    for database in databases:
-        print(database['id'])
-
-
-def delete_database(client, id):
-    print("\n5. Delete Database")
-
-    try:
-        client.delete_database(id)
-
-        print('Database with id \'{0}\' was deleted'.format(id))
-
-    except exceptions.CosmosResourceNotFoundError:
-        print('A database with id \'{0}\' does not exist'.format(id))
-
-
-def run_sample():
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
-    try:
-        # query for a database
-        find_database(client, DATABASE_ID)
-
-        # create a database
-        create_database(client, DATABASE_ID)
-
-        # get a database using its id
-        read_database(client, DATABASE_ID)
-
-        # list all databases on an account
-        list_databases(client)
-
-        # delete database by id
-        delete_database(client, DATABASE_ID)
-
-    except exceptions.CosmosHttpResponseError as e:
-        print('\nrun_sample has caught an error. {0}'.format(e.message))
-
-    finally:
-        print("\nrun_sample done")
+def main():
+    database = Cosmos_DB(HOST,MASTER_KEY,DATABASE_ID,CONTAINER_ID)
+    database.upsert_item({ 'id' : '1',
+                        'last_accon_time': '2013-10-25 04:17:05',
+                        'UTC_Time': '2013-10-25 04:18:05', 
+                        'total_trip_mileage': 1151388, 
+                        'current_trip_mileage': 0, 
+                        'total_fuel': 33641, 
+                        'current_fuel': 12, 
+                        'vstate': '00000000', 
+                        'reserved': '036401014C000300'})
 
 if __name__ == '__main__':
-    run_sample()
+    main()
 
 #Send Delete Receive, Class 
+#Question to Ask
+# what should be our partitionKey
